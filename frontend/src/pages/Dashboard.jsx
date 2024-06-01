397e4caf513df22b41208e46ed68c5002ee81d61
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-06-01 15:02:51 +0200

    Design and implement main dashboard components.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,17 @@
-import { Logout } from "./Logout";
+import { Sidebar } from "../components/Sidebar"
+import { PointsCard } from "../components/PointsCard"
+import { ActivityGraph } from "../components/ActivityGraph"
+import { DistanceCard } from "../components/DistanceCard"
+import { EnergyCard } from "../components/EnergyCard"
 
 export const Dashboard = () => {
-
-  return (<div>
-    Dashboard
-    <Logout />
-  </div>
+  return (
+    <>
+      <Sidebar />
+      <PointsCard />
+      <ActivityGraph />
+      <DistanceCard />
+      <EnergyCard />
+    </>
   )
-};
+}
