5060b2359d7e188f08bad1e44649be302e1e2aed
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-06-02 20:04:25 +0200

    functionality to fetch and display energy level

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,125 +3,126 @@ import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 
 const Container = styled.div`
-display: flex;
-width: 100vw;
-background-color: #fff;
-`;
+  display: flex;
+  width: 100vw;
+  background-color: #fff;
+`
 
 const LeftColumn = styled.div`
-flex: 1;
-display: flex;
-justify-content: center;
-align-items: center;
-padding: 20px;
-box-sizing: border-box;
-background-color: #d9d9d9;
-border-radius: 20px;
-`;
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  background-color: #d9d9d9;
+  border-radius: 20px;
+`
 
 const RightColumn = styled.div`
-flex: 1;
-padding: 20px;
-box-sizing: border-box;
-display: flex;
-flex-direction: column;
-justify-content: center;
-background-color: #ffffff;
-`;
+  flex: 1;
+  padding: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+`
 
 const ImageContainer = styled.div`
-width: 80%;
-text-align: center;
-`;
+  width: 80%;
+  text-align: center;
+`
 
 const StyledImage = styled.img`
-max-width: 100%;
-height: auto;
-`;
+  max-width: 100%;
+  height: auto;
+`
 
 const FormContainer = styled.div`
-width: 100%;
-max-width: 400px;
-margin: 0 auto;
-h2 {
-  text-align: center;
-}
-`;
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+  h2 {
+    text-align: center;
+  }
+`
 
 const FormGroup = styled.div`
-margin-bottom: 15px;
-`;
+  margin-bottom: 15px;
+`
 
 const Input = styled.input`
-outline: none;
-border: none;
-width: 100%;
-padding: 10px;
-color: black;
-box-sizing: border-box;
-background-color: #d9d9d9;
-border-radius: 10px; //adjust once design is done
+  outline: none;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: black;
+  box-sizing: border-box;
+  background-color: #d9d9d9;
+  border-radius: 10px; //adjust once design is done
 
-&:focus {
-  background-color: #fff;
-  border: 1px solid black;
-}
-`;
+  &:focus {
+    background-color: #fff;
+    border: 1px solid black;
+  }
+`
 
 const Button = styled.button`
-width: 100%;
-padding: 10px;
-background-color: #88a183;
-border: none;
-border-radius: 20px; //adjust once design is done
-color: black; //  white or black?
-cursor: pointer;
-&:hover {
-  background-color: #88a183b7;
-}
-`;
+  width: 100%;
+  padding: 10px;
+  background-color: #88a183;
+  border: none;
+  border-radius: 20px; //adjust once design is done
+  color: black; //  white or black?
+  cursor: pointer;
+  &:hover {
+    background-color: #88a183b7;
+  }
+`
 
 const ForgotPassword = styled.a`
-display: block;
-margin-top: 10px;
-text-align: right;
-font-size: 0.6em;
-color: black;
-text-decoration: none;
-font-weight: bold;
-&:hover {
-  //some effects
-}
-`;
+  display: block;
+  margin-top: 10px;
+  text-align: right;
+  font-size: 0.6em;
+  color: black;
+  text-decoration: none;
+  font-weight: bold;
+  &:hover {
+    //some effects
+  }
+`
 
 const BottomText = styled.div`
-margin-top: 20px;
-font-size: 0.6em;
-color: black;
-p {
-text-align: center;
-margin: 5px 0;
-}
-a {
-font-weight: bold;
-color: black;
-text-decoration: none;
+  margin-top: 20px;
+  font-size: 0.6em;
+  color: black;
+  p {
+    text-align: center;
+    margin: 5px 0;
+  }
+  a {
+    font-weight: bold;
+    color: black;
+    text-decoration: none;
 
-&:hover {
-  //some effects
-}
-}
+    &:hover {
+      //some effects
+    }
+  }
 `
 export const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
-  const apikey = import.meta.env.VITE_API_KEY;
-  const API = `${apikey}/sessions`;
-  const navigate = useNavigate();
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [message, setMessage] = useState("")
+  const apikey = import.meta.env.VITE_API_KEY
+  const API = `${apikey}/sessions`
+
+  const navigate = useNavigate()
 
   const handleLogin = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     fetch(API, {
       method: "POST",
       body: JSON.stringify({ email, password }),
@@ -129,24 +130,24 @@ export const Login = () => {
     })
       .then((res) => {
         if (!res.ok) {
-          throw new Error("Invalid login");
+          throw new Error("Invalid login")
         }
-        return res.json();
+        return res.json()
       })
       .then((data) => {
-        console.log(data);
-        setMessage("Login successful");
-        sessionStorage.setItem("accessToken", data.accessToken);
-        navigate("/dashboard");
+        console.log(data)
+        setMessage("Login successful")
+        sessionStorage.setItem("id", data._id)
+        sessionStorage.setItem("accessToken", data.accessToken)
+        const id = sessionStorage.getItem("id")
+        console.log(data)
+        navigate(`/users/${id}`)
       })
       .catch((error) => {
-        console.error(error);
-        setMessage("Failed to login");
-      });
-  };
-
-
-
+        console.error(error)
+        setMessage("Failed to login")
+      })
+  }
 
   return (
     <Container>
@@ -200,6 +201,3 @@ export const Login = () => {
     </Container>
   )
 }
-
-
-
--- frontend/src/reusables/BatterySlider.jsx
+++ frontend/src/reusables/BatterySlider.jsx
@@ -1,21 +1,66 @@
 import styled from "styled-components"
+import { useParams } from "react-router"
 import { BatterySVG } from "../reusables/BatterySVG"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { Button } from "@mui/material"
+const apikey = import.meta.env.VITE_API_KEY
 
 const BatterySliderWrapper = styled.div`
   width: 195px;
 `
 
 export const BatterySlider = () => {
-  const [fillPercentage, setFillPercentage] = useState(10) //set initial state from the EnergyLevel in the DB
+  const [fillPercentage, setFillPercentage] = useState(0) //set initial state from the EnergyLevel in the DB
+  const { id } = useParams()
+  const API = `${apikey}/user/${id}`
+  const API_ENERGY = `${apikey}/energy`
+  const token = sessionStorage.getItem("accessToken")
+
+  useEffect(() => {
+    const fetchEnergyData = async () => {
+      try {
+        const res = await fetch(API)
+        if (!res.ok) {
+          throw new Error("Failed to fetch data")
+        }
+        const data = await res.json()
+        setFillPercentage(data.energyLevel)
+      } catch (error) {
+        console.error("Error fetching energy data:", error)
+      }
+    }
+    fetchEnergyData()// this work fine 
+  }, [API, id])
 
   const handleDrag = (percentage) => {
     setFillPercentage(percentage)
   }
 
+  const handleNewEnergy = async () => {
+    try {
+      console.log(token)
+      const response = await fetch(API_ENERGY, {
+        method: "PATCH",
+        body: JSON.stringify({ energyLevel: fillPercentage }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,// this doesn't
+        },
+      })
+      if (!response.ok) {
+        throw new Error("Failed to update data")
+      }
+      const updatedData = await response.json()
+      setFillPercentage(updatedData.energyLevel)
+    } catch (error) {
+      console.error("Error updating energy data:", error)
+    }
+  }
+
   return (
     <BatterySliderWrapper>
       <BatterySVG fillPercentage={fillPercentage} onDrag={handleDrag} />
+      <Button onClick={handleNewEnergy}>Confirm</Button>
     </BatterySliderWrapper>
   )
 }
