eead102d7cc810939554e5208daba26a4c09f555
Vittoria Matteoli <info@vittoriamatteoli.com>
2024-06-06 12:03:00 +0200

    started dashboard styling +points card

--- frontend/src/components/PointsCard.jsx
+++ frontend/src/components/PointsCard.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  text-align: right;
+  h2 {
+    text-align: left;
+  }
+`;
+
 export const PointsCard = () => {
-  return <>Here goes our points counter.</>
-}
+const [points,setPoints]=useState("")
+
+useEffect(()=>{
+  
+})
+
+
+  return (
+    <StyledWrapper>
+      <h2>Dashboard</h2>
+      <a>
+        Your Points
+        <span>
+          <img src="/info.svg" alt="informations-icon" />
+        </span>
+      </a>
+      <hr />
+      <h3>points</h3>
+    </StyledWrapper>
+  );
+};
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,21 +1,35 @@
-import { Sidebar } from "../components/Sidebar"
-import { PointsCard } from "../components/PointsCard"
-import { ActivityGraph } from "../components/ActivityGraph"
-import { DistanceCard } from "../components/DistanceCard"
-import { EnergyCard } from "../components/EnergyCard"
-import { useParams } from "react-router-dom"
+import { Sidebar } from "../components/Sidebar";
+import { PointsCard } from "../components/PointsCard";
+import { ActivityGraph } from "../components/ActivityGraph";
+import { DistanceCard } from "../components/DistanceCard";
+import { EnergyCard } from "../components/EnergyCard";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const DashboardLayout = styled.div`
+  box-sizing: border-box;
+  padding: 20px 12px;
+  display: grid;
+  grid-template-columns: auto auto auto; /* Sidebar and main content */
+  width: 100%;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr; /* Single column layout on mobile */
+  }
+`;
 
 export const Dashboard = () => {
-
   const { id } = useParams();
 
   return (
-    <>
+    <DashboardLayout>
       <Sidebar id={id} />
-      <PointsCard id={id} />
-      <ActivityGraph id={id} />
-      <DistanceCard id={id} />
-      <EnergyCard id={id} />
-    </>
-  )
-}
+      <div>
+        <PointsCard id={id} />
+        <ActivityGraph id={id} />
+        <DistanceCard id={id} />
+        <EnergyCard id={id} />
+      </div>
+    </DashboardLayout>
+  );
+};
